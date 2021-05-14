e24385b124485884084699a460d1510cda7f7ff8
Duc Cao <caovanducs@gmail.com>
2021-05-14 16:32:09 +0700

    Onboard facebook chatbot

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const chalk = require("chalk");
-
+require("dotenv").config();
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/api/hello-world", function (req, res) {
   res.json({
     data: "Hello World!",
   });
 });
+// API Service
+app.use("/api/webhook", require("./routes/webhook.route"));
 
 const PORT = process.env.PORT || 1212;
 app.listen(PORT, function () {
