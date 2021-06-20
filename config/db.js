ca85ccc099a3ec03a106b5cf88cd3fada644ce25
Duc Cao <caovanducs@gmail.com>
2021-06-20 11:22:59 +0700

    Deploy try 2

--- config/db.js
+++ config/db.js
@@ -1,16 +1,29 @@
-const mysql = require("mysql");
-const util = require("util");
+const mysql = require('mysql');
+const util = require('util');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || "3306",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "SPA_ONLINE_ACADEMY",
-  connectionLimit: 50,
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || '3306',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'SPA_ONLINE_ACADEMY',
+  connectionLimit: 50
 });
 
-const pool_query = util.promisify(pool.query).bind(pool);
+const herokuClearDB = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  // port: process.env.DB_PORT || '3306',
+  user: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_DATABASE_NAME || 'SPA_ONLINE_ACADEMY',
+  connectionLimit: 50
+});
+
+// local
+// const pool_query = util.promisify(pool.query).bind(pool);
+
+// development
+const pool_query = util.promisify(herokuClearDB.query).bind(herokuClearDB);
 
 module.exports = {
   load: (sql) => {
@@ -22,17 +35,17 @@ module.exports = {
   edit: (entity, condition, table_name) => {
     return pool_query(`update ${table_name} set ? where ?`, [
       entity,
-      condition,
+      condition
     ]);
   },
   editTwoCons: (entity, condition, andCondition, table_name) => {
     return pool_query(`update ${table_name} set ? where ? and ?`, [
       entity,
       condition,
-      andCondition,
+      andCondition
     ]);
   },
   del: (condition, table_name) => {
     return pool_query(`delete from ${table_name} where ?`, condition);
-  },
+  }
 };
